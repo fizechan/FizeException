f820cac6470f632db3f9cd89c778c71db2a9f104
Fize <chenfengzhan@qq.com>
2022-08-17 13:13:56 +0800

    新增HttpRequestException特性。

--- src/HttpRequestException.php
+++ src/HttpRequestException.php
@@ -3,10 +3,19 @@
 namespace Fize\Exception;
 
 use Fize\Http\RequestException;
+use Fize\Web\Request;
 
 /**
  * 异常：HTTP请求异常
  */
 class HttpRequestException extends RequestException
 {
+    /**
+     * 获取访问的 URL
+     * @return string
+     */
+    public function url(): string
+    {
+        return Request::url();
+    }
 }
