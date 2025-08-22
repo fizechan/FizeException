d4b4d7d0ca41d32edaa3ef901b78b0b7a82d0e4d
Fize <chenfengzhan@qq.com>
2025-08-22 16:20:06 +0800

    feat: 增强FileException类。

--- src/FileException.php
+++ src/FileException.php
@@ -2,9 +2,39 @@
 
 namespace Fize\Exception;
 
+use Throwable;
+
 /**
  * 文件异常
  */
 class FileException extends RuntimeException
 {
+
+    /**
+     * @var string 文件路径
+     */
+    protected $filePath;
+
+    /**
+     * 初始化
+     * @param string         $filePath 文件路径
+     * @param string         $message  错误描述
+     * @param int            $code     错误码
+     * @param Throwable|null $previous 前置错误
+     */
+    public function __construct($filePath, $message = "", $code = 0, Throwable $previous = null)
+    {
+        $this->filePath = $filePath;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * 获取文件路径
+     * @return string
+     */
+    public function getFilePath(): string
+    {
+        return $this->filePath;
+    }
+
 }
--- tests/TestFileException.php
+++ tests/TestFileException.php
@@ -9,7 +9,7 @@ class TestFileException extends TestCase
 {
     public function test__construct()
     {
-        $exception = new FileException();
+        $exception = new FileException(__FILE__);
         var_dump($exception);
         self::assertInstanceOf(FileException::class, $exception);
     }
